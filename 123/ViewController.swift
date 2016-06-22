75894b285834ab674e39a11e0e5c0985381a7c24
gmreason <453948208@qq.com>
2016-06-22 16:44:57 +0800

    Initial Commit

---
+++ 123/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  123
+//
+//  Created by gmr on 16/6/22.
+//  Copyright © 2016年 gmr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

b07de724771f2a32958edbd89b6a5daa56536fc9
20161104610 <1663512557@qq.com>
2018-12-26 11:01:40 +0800

    Initial commit

--- 123/ViewController.swift
+++ 123/ViewController.swift
@@ -10,6 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var z: UITextField!
+    @IBAction func add(sender: AnyObject) {
+        var temp=0
+        temp=(Int)(x.text!)!+(Int)(y.text!)!
+        z.text="\(temp)"
+    }
+    @IBAction func minus(sender: AnyObject) {
+        var temp=0
+        temp=(Int)(x.text!)!-(Int)(y.text!)!
+        z.text="\(temp)"
+    }
+    @IBAction func multiply(sender: AnyObject) {
+        var temp=0
+        temp=(Int)(x.text!)!*(Int)(y.text!)!
+        z.text="\(temp)"
+    }
+    @IBAction func division(sender: AnyObject) {
+        var temp=0
+        temp=(Int)(x.text!)!/(Int)(y.text!)!
+        z.text="\(temp)"
+    }
+    @IBAction func c0(sender: AnyObject) {
+    }
+    @IBAction func c1(sender: AnyObject) {
+    }
+    @IBAction func c2(sender: AnyObject) {
+    }
+    @IBAction func c3(sender: AnyObject) {
+    }
+    @IBAction func c4(sender: AnyObject) {
+    }
+    @IBAction func c5(sender: AnyObject) {
+    }
+    @IBAction func c6(sender: AnyObject) {
+    }
+    @IBAction func c7(sender: AnyObject) {
+    }
+    @IBAction func c8(sender: AnyObject) {
+    }
+    @IBAction func c9(sender: AnyObject) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
